da34b2db321afdafcc20bffb2adc01dafd16701d
Nicolas Devel <nicolassabogal85@gmail.com>
2023-03-16 17:17:14 -0500

    feat sensor print

--- android/app/src/main/kotlin/com/example/flutter_many_functions/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_many_functions/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flutter_many_functions
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
