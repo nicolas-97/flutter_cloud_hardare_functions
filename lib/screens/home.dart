8490d728ffb0e3fd5264e0f96f98716d385d707a
Nicolas Devel <nicolassabogal85@gmail.com>
2023-03-16 16:31:57 -0500

    progress

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_many_functions/components/alert_dialogs.dart';
 import 'package:flutter_many_functions/models/responses/user.dart';
 import 'package:flutter_many_functions/providers/user_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:local_auth/local_auth.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,29 +14,156 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final LocalAuthentication _auth = LocalAuthentication();
+  _SupportState _supportState = _SupportState.unknown;
+  bool? _canCheckBiometrics;
+  List<BiometricType>? _availableBiometrics;
+  String _authorized = 'Not Authorized';
+  bool _isAuthenticating = false;
 
   final _formKey = GlobalKey<FormState>();
   User userCredentials = User();
   bool _showPassword = false;
 
-  _changeShowPassword(){
+  _changeShowPassword() {
     setState(() {
       _showPassword = !_showPassword;
     });
   }
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    _auth.isDeviceSupported().then(
+          (bool isSupported) => setState(() => _supportState = isSupported
+              ? _SupportState.supported
+              : _SupportState.unsupported),
+        );
+  }
 
+  Future<void> _checkBiometrics() async {
+    late bool canCheckBiometrics;
+    try {
+      canCheckBiometrics = await _auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      canCheckBiometrics = false;
+      print(e);
+    }
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _canCheckBiometrics = canCheckBiometrics;
+    });
+  }
+
+  Future<void> _getAvailableBiometrics() async {
+    late List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await _auth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      availableBiometrics = <BiometricType>[];
+      print(e);
+    }
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _availableBiometrics = availableBiometrics;
+    });
+  }
+
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+    try {
+      setState(() {
+        _isAuthenticating = true;
+        _authorized = 'Authenticating';
+      });
+      authenticated = await _auth.authenticate(
+        localizedReason: 'Let OS determine authentication method',
+        options: const AuthenticationOptions(
+          stickyAuth: true,
+        ),
+      );
+      setState(() {
+        _isAuthenticating = false;
+      });
+    } on PlatformException catch (e) {
+      print(e);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = 'Error - ${e.message}';
+      });
+      return;
+    }
+    if (!mounted) {
+      return;
+    }
+
+    setState(
+        () => _authorized = authenticated ? 'Authorized' : 'Not Authorized');
+  }
+
+  Future<void> _authenticateWithBiometrics() async {
+    bool authenticated = false;
+    try {
+      setState(() {
+        _isAuthenticating = true;
+        _authorized = 'Authenticating';
+      });
+      authenticated = await _auth.authenticate(
+        localizedReason:
+            'Scan your fingerprint (or face or whatever) to authenticate',
+        options: const AuthenticationOptions(
+          stickyAuth: true,
+          biometricOnly: true,
+        ),
+      );
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = 'Authenticating';
+      });
+    } on PlatformException catch (e) {
+      print(e);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = 'Error - ${e.message}';
+      });
+      return;
+    }
+    if (!mounted) {
+      return;
+    }
+
+    final String message = authenticated ? 'Authorized' : 'Not Authorized';
+    setState(() {
+      _authorized = message;
+    });
+  }
+
+  Future<void> _cancelAuthentication() async {
+    await _auth.stopAuthentication();
+    setState(() => _isAuthenticating = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final UserProvider userProvider = Provider.of<UserProvider>(context);
     final AlertsDialogs alertsDialogs = AlertsDialogs(context);
 
-    return Scaffold(
-      body: Padding(
+    if (_supportState == _SupportState.unknown) {
+      return const Scaffold(
+        body: CircularProgressIndicator(),
+      );
+    } else {
+      return Scaffold(
+          body: Padding(
         padding: EdgeInsets.symmetric(
-          vertical: MediaQuery.of(context).size.height / 8, 
-          horizontal: MediaQuery.of(context).size.width / 7
-          ),
+            vertical: MediaQuery.of(context).size.height / 8,
+            horizontal: MediaQuery.of(context).size.width / 7),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -55,15 +184,14 @@ class _HomeState extends State<Home> {
                     ),
                     TextFormField(
                       decoration: InputDecoration(
-                        label: const Text('Password'),
-                        suffix: IconButton(
-                          iconSize: 17,
-                          icon: _showPassword == true 
-                          ? const Icon(Icons.visibility_off) 
-                          : const Icon(Icons.visibility),
-                          onPressed: () => _changeShowPassword(),
-                        )
-                      ),
+                          label: const Text('Password'),
+                          suffix: IconButton(
+                            iconSize: 17,
+                            icon: _showPassword == true
+                                ? const Icon(Icons.visibility_off)
+                                : const Icon(Icons.visibility),
+                            onPressed: () => _changeShowPassword(),
+                          )),
                       obscureText: _showPassword,
                       onChanged: (value) => userCredentials.password = value,
                     ),
@@ -71,9 +199,10 @@ class _HomeState extends State<Home> {
                       child: const Text('login'),
                       onPressed: () async {
                         await userProvider.login(userCredentials.credentials());
-                        if(userProvider.error.message != null){
-                          await alertsDialogs.errorLogin(userProvider.error.message!);
-                        }else{
+                        if (userProvider.error.message != null) {
+                          await alertsDialogs
+                              .errorLogin(userProvider.error.message!);
+                        } else {
                           //pasar de pantalla
                           print("Me he logueado correctamente");
                         }
@@ -86,10 +215,60 @@ class _HomeState extends State<Home> {
             TextButton(
               child: const Text('Create account'),
               onPressed: () => Navigator.pushNamed(context, '/create-account'),
-            )
+            ),
+            const Divider(height: 100),
+                Text('Current State: $_authorized\n'),
+                if (_isAuthenticating)
+                  ElevatedButton(
+                    onPressed: _cancelAuthentication,
+                    // TODO(goderbauer): Make this const when this package requires Flutter 3.8 or later.
+                    // ignore: prefer_const_constructors
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const <Widget>[
+                        Text('Cancel Authentication'),
+                        Icon(Icons.cancel),
+                      ],
+                    ),
+                  )
+                  else
+                  Column(
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: _authenticate,
+                        // TODO(goderbauer): Make this const when this package requires Flutter 3.8 or later.
+                        // ignore: prefer_const_constructors
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: const <Widget>[
+                            Text('Authenticate'),
+                            Icon(Icons.perm_device_information),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: _authenticateWithBiometrics,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(_isAuthenticating
+                                ? 'Cancel'
+                                : 'Authenticate: biometrics only'),
+                            const Icon(Icons.fingerprint),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
           ],
         ),
-      )
-    );
+      ));
+    }
   }
+}
+
+enum _SupportState {
+  unknown,
+  supported,
+  unsupported,
 }
